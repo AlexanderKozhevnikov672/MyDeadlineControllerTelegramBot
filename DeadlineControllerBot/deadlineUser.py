fb4c14d95104c03bf62cee3062a5f9b1470e3bdc
Alexander_Kozhevnikov <kozhevnikov.av@phystech.edu>
2023-05-02 18:46:56 +0300

    fixed code style

--- DeadlineControllerBot/deadlineUser.py
+++ DeadlineControllerBot/deadlineUser.py
@@ -23,49 +23,49 @@ class DeadlineUser:
         self.deadlines = list()
         self.size = 0
         self.subscriptions = set()
-    
-    def findEventName(self, name : str):
+
+    def findEventName(self, name: str):
         for event in self.deadlines:
             if name == event.name:
                 return True
         return False
-    
+
     def addNewEvent(self, name: str, date: datetime.date):
         event = Event(name, date)
         self.deadlines.append(event)
         self.size += 1
-    
+
     def delEvent(self, name: str):
         for i in range(len(self.deadlines)):
             if self.deadlines[i].name == name:
                 self.deadlines.pop(i)
                 self.size -= 1
                 return
-        
+
     def getEarliestEvents(self, num: int):
         num = min(num, self.size)
         self.deadlines.sort()
         return self.deadlines[:num]
-    
+
     def changeEventDate(self, name: str, newDate: datetime.date):
         for i in range(len(self.deadlines)):
             if self.deadlines[i].name == name:
                 self.deadlines[i].date = newDate
                 return
-            
+
     def changeEventName(self, oldName: str, newName: str):
         for i in range(len(self.deadlines)):
             if self.deadlines[i].name == oldName:
                 self.deadlines[i].name = newName
                 return
-    
+
     def getDateEvents(self, date: datetime.date):
         result = list()
         for event in self.deadlines:
             if event.date == date:
                 result.append(event)
         return result
-    
+
     def clearEvents(self):
         self.deadlines.clear()
 
@@ -80,12 +80,12 @@ class DeadlineUser:
         for event in self.deadlines:
             if event.name == name:
                 return event.date
-    
+
     def subscribe(self, userId):
         self.subscriptions.add(userId)
-    
+
     def unsubscribe(self, userId):
         self.subscriptions.discard(userId)
-    
+
     def findSubscription(self, userId):
         return userId in self.subscriptions
